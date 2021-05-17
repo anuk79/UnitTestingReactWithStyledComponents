bfae46ced13af8a76ba2fc308e384e719467c21a
Anuradha Kumari <32791199+anuk79@users.noreply.github.com>
2021-05-17 20:31:14 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Test application for showcasing example for unit testing react code using styled components
+        Test new changes application for showcasing example for unit testing
+        react code using styled components
       </div>
     );
   }
